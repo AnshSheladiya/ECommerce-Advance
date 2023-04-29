1193cd6365524f6baf0db0d3f23fb67e9087998a
AnshSheladiya <anshsheladiya83@gmail.com>
2023-04-29 16:16:33 +0530

    ECA-10:Test Commit 3

--- backend/seeds/seed-products.js
+++ backend/seeds/seed-products.js
@@ -1,12 +1,5 @@
 const products = [
   {
-    "product_name": "The Great Gatsby",
-    "description": "A classic novel by F. Scott Fitzgerald",
-    "price": 12.99,
-    "quantity": 50,
-    "category_id": "64455e239fddc173165ddcf4"
-  },
-  {
     "product_name": "iPhone 13 Pro",
     "description": "Apple's latest flagship smartphone",
     "price": 999.99,
@@ -14,32 +7,12 @@ const products = [
     "category_id": "64455e239fddc173165ddcf5"
   },
   {
-    "product_name": "Levi's 501 Jeans",
-    "description": "The original button-fly jeans",
-    "price": 49.99,
+    "product_name": "Imac laptop",
+    "description": "The original Laptop",
+    "price": 2000.99,
     "quantity": 30,
     "category_id": "64455e239fddc173165ddcf6"
   },
-  {
-    "product_name": "Instant Pot Duo Nova",
-    "description": "A versatile pressure cooker and slow cooker",
-    "price": 79.99,
-    "quantity": 15,
-    "category_id": "64455e239fddc173165ddcf7"
-  },
-  {
-    "product_name": "Nintendo Switch",
-    "description": "A hybrid gaming console for home and on-the-go",
-    "price": 299.99,
-    "quantity": 25,
-    "category_id": "64455e239fddc173165ddcf8"
-  },
-  {
-    "product_name": "Fitbit Charge 5",
-    "description": "A fitness tracker with built-in GPS",
-    "price": 179.99,
-    "quantity": 35,
-    "category_id": "64455e239fddc173165ddcf9"
-  }
+
 ];
 module.exports=products
