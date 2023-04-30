b9fe091837bca047bde6d5fc1ee74995893f7644
AnshSheladiya <anshsheladiya83@gmail.com>
2023-04-30 13:24:12 +0530

    ECA-10:Test Commit 13

--- backend/src/api/routes/userRoutes.js
+++ backend/src/api/routes/userRoutes.js
@@ -5,10 +5,10 @@
  const router = express.Router();
  const userController = require('../controllers/userController');
  const authMiddleware = require('../middlewares/authMiddleware');
- 
+
 //  router.post('/profile', authMiddleware, userController.createUserProfile);
  router.get('/profile', authMiddleware, userController.getUserProfile);
  router.get('/users', userController.getUser);
 //  router.put('/profile', authMiddleware, userController.updateUserProfile);
- 
+
  module.exports = router;
--- backend/src/api/services/userServices.js
+++ backend/src/api/services/userServices.js
@@ -2,20 +2,20 @@
   * File Name: userServices.js
   */
   const User = require('../models/user');
- 
-  exports.createUserProfile = async (userId, userProfileData) => {
-    try {
-      const user = await User.findByIdAndUpdate(
-        userId,
-        { $set: { profile: userProfileData } },
-        { new: true }
-      );
-      return user.profile;
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  };
-  
+
+//   exports.createUserProfile = async (userId, userProfileData) => {
+//     try {
+//       const user = await User.findByIdAndUpdate(
+//         userId,
+//         { $set: { profile: userProfileData } },
+//         { new: true }
+//       );
+//       return user.profile;
+//     } catch (error) {
+//       throw new Error(error.message);
+//     }
+//   };
+
   exports.getUserProfile = async (userId) => {
     try {
       const user = await User.findById(userId);
@@ -32,88 +32,88 @@
       throw new Error(error.message);
     }
   };
-  
-  exports.updateUserProfile = async (userId, userProfileData) => {
-    try {
-      const user = await User.findByIdAndUpdate(
-        userId,
-        { $set: { profile: userProfileData } },
-        { new: true }
-      );
-      return user.profile;
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  };
 
-exports.getAddresses = async (userId) => {
-  const user = await User.findById(userId);
-  return {
-    shipping_addresses: user.shipping_address_book,
-    billing_addresses: user.billing_address_book,
-  };
-};
+//   exports.updateUserProfile = async (userId, userProfileData) => {
+//     try {
+//       const user = await User.findByIdAndUpdate(
+//         userId,
+//         { $set: { profile: userProfileData } },
+//         { new: true }
+//       );
+//       return user.profile;
+//     } catch (error) {
+//       throw new Error(error.message);
+//     }
+//   };
 
-exports.updateAddress = async (userId, addressId, update) => {
-  const user = await User.findById(userId);
-  const { shipping_address_book, billing_address_book } = user;
+// exports.getAddresses = async (userId) => {
+//   const user = await User.findById(userId);
+//   return {
+//     shipping_addresses: user.shipping_address_book,
+//     billing_addresses: user.billing_address_book,
+//   };
+// };
 
-  let updatedAddress = null;
-  let foundAddress = false;
+// exports.updateAddress = async (userId, addressId, update) => {
+//   const user = await User.findById(userId);
+//   const { shipping_address_book, billing_address_book } = user;
 
-  shipping_address_book.forEach((address) => {
-    if (address._id.toString() === addressId) {
-      foundAddress = true;
-      Object.assign(address, update);
-      updatedAddress = address;
-    }
-  });
+//   let updatedAddress = null;
+//   let foundAddress = false;
 
-  if (!foundAddress) {
-    billing_address_book.forEach((address) => {
-      if (address._id.toString() === addressId) {
-        foundAddress = true;
-        Object.assign(address, update);
-        updatedAddress = address;
-      }
-    });
-  }
+//   shipping_address_book.forEach((address) => {
+//     if (address._id.toString() === addressId) {
+//       foundAddress = true;
+//       Object.assign(address, update);
+//       updatedAddress = address;
+//     }
+//   });
 
-  if (!foundAddress) {
-    throw new Error('Address not found.');
-  }
+//   if (!foundAddress) {
+//     billing_address_book.forEach((address) => {
+//       if (address._id.toString() === addressId) {
+//         foundAddress = true;
+//         Object.assign(address, update);
+//         updatedAddress = address;
+//       }
+//     });
+//   }
 
-  await user.save();
-  return updatedAddress;
-};
+//   if (!foundAddress) {
+//     throw new Error('Address not found.');
+//   }
 
-exports.addAddress = async (userId, address) => {
-  const user = await User.findById(userId);
-  if (address.label.toLowerCase() === 'shipping') {
-    user.shipping_address_book.push(address);
-  } else if (address.label.toLowerCase() === 'billing') {
-    user.billing_address_book.push(address);
-  } else {
-    throw new Error('Invalid address label.');
-  }
-  await user.save();
-  return address;
-};
+//   await user.save();
+//   return updatedAddress;
+// };
 
-exports.deleteAddress = async (userId, addressId) => {
-  const user = await User.findById(userId);
-  const { shipping_address_book, billing_address_book } = user;
+// exports.addAddress = async (userId, address) => {
+//   const user = await User.findById(userId);
+//   if (address.label.toLowerCase() === 'shipping') {
+//     user.shipping_address_book.push(address);
+//   } else if (address.label.toLowerCase() === 'billing') {
+//     user.billing_address_book.push(address);
+//   } else {
+//     throw new Error('Invalid address label.');
+//   }
+//   await user.save();
+//   return address;
+// };
 
-  let addressIndex = shipping_address_book.findIndex((address) => address._id.toString() === addressId);
-  if (addressIndex !== -1) {
-    shipping_address_book.splice(addressIndex, 1);
-  } else {
-    addressIndex = billing_address_book.findIndex((address) => address._id.toString() === addressId);
-    if (addressIndex !== -1) {
-      billing_address_book.splice(addressIndex, 1);
-} else {
-throw new Error('Address not found.');
-}
-}
-await user.save();
-};
+// exports.deleteAddress = async (userId, addressId) => {
+//   const user = await User.findById(userId);
+//   const { shipping_address_book, billing_address_book } = user;
+
+//   let addressIndex = shipping_address_book.findIndex((address) => address._id.toString() === addressId);
+//   if (addressIndex !== -1) {
+//     shipping_address_book.splice(addressIndex, 1);
+//   } else {
+//     addressIndex = billing_address_book.findIndex((address) => address._id.toString() === addressId);
+//     if (addressIndex !== -1) {
+//       billing_address_book.splice(addressIndex, 1);
+// } else {
+// throw new Error('Address not found.');
+// }
+// }
+// await user.save();
+// };
